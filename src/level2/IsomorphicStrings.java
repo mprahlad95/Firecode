f04d934404999deac6c59263adbab2444006f765
Prahlad Misra <mprahlad95@gmail.com>
2018-07-09 17:38:28 +0530

    added

--- src/level2/IsomorphicStrings.java
+++ src/level2/IsomorphicStrings.java
@@ -1,20 +1,23 @@
 package level2;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class IsomorphicStrings {
 	public static boolean isIsomorphic(String input1, String input2) {
-	    HashMap<Character, Integer> map1 = new HashMap<>();
-	    HashMap<Character, Integer> map2 = new HashMap <>();
-	    if(input1.length() != input2.length()) return false;
-	    for(int i=0; i<input1.length(); i++){
-	        char ch=input1.charAt(i);
-	        if(map1.containsKey(ch)) map1.put(ch, map1.get(ch)+1);
-	        else map1.put(ch, 1);
-	        char ch1=input2.charAt(i);
-	        if(map2.containsKey(ch1)) map2.put(ch1, map2.get(ch1)+1);
-	        else map2.put(ch1, 1);
-	    }
-	    return map1.values().containsAll(map2.values()) && map2.values().containsAll(map1.values());
+		if (input1.length() != input2.length())
+			return false;
+		Map<Character, Character> freq1 = new HashMap<>();
+		Map<Character, Character> freq2 = new HashMap<>();
+		for (int i = 0; i < input1.length(); i++) {
+			char c1 = input1.charAt(i);
+			char c2 = input2.charAt(i);
+			if (!freq1.containsKey(c1))
+				freq1.put(c1, c2);
+			if (!freq2.containsKey(c2))
+				freq2.put(c2, c1);
+			if (freq1.get(c1) != c2 || freq2.get(c2) != c1)
+				return false;
+		}
+		return true;
 	}
 }
--- src/level3/MaximumSumPathBT.java
+++ src/level3/MaximumSumPathBT.java
@@ -2,57 +2,19 @@ package level3;
 
 public class MaximumSumPathBT {
 	public static int maxSumPath(TreeNode root) {
-		int[] maxRecursiveHolder = new int[1];
-		maxSumPathMain(root, maxRecursiveHolder);
-		return maxRecursiveHolder[0];
+		int max[] = new int[1];
+		max[0] = 0;
+		calculateSum(root, max);
+		return max[0];
 	}
 
-	public static int maxSumPathMain(TreeNode root, int[] maxRecursiveHolder) {
+	public static int calculateSum(TreeNode root, int[] max) {
 		if (root == null)
 			return 0;
-		int leftSum = maxSumPathMain(root.left, maxRecursiveHolder);
-		int rightSum = maxSumPathMain(root.right, maxRecursiveHolder);
-		// Get the max path sum up to this node, including this node's value
-		int nodeCumVal = Math.max(root.data + leftSum, root.data + rightSum);
-		// Check and update the max holder
-		maxRecursiveHolder[0] = Math.max(maxRecursiveHolder[0], leftSum + root.data + rightSum);
-		return nodeCumVal;
+		int left = calculateSum(root.left, max);
+		int right = calculateSum(root.right, max);
+		int current = Math.max(root.data, Math.max(root.data + left, root.data + right));
+		max[0] = Math.max(max[0], Math.max(current, left + root.data + right));
+		return current;
 	}
 }
-
-/* public int maxPathSum(TreeNode root) {
-if (root == null) return 0;
-ans = root.val;
-getMax(root);
-return ans;
-}
-
-private int getMax(TreeNode node) {
-if (node == null) return 0;
-
-int left = getMax(node.left);
-int right = getMax(node.right);
-
-int maxPathThruCurrent; //max path with current node as root 
-int toReturn; //max between node.val, node.val + left, node.val + right
-
-if (left <= 0 && right <= 0){
-    maxPathThruCurrent = node.val;
-    toReturn = node.val;
-}
-else if (left <= 0) {
-    maxPathThruCurrent = node.val + right;
-    toReturn = node.val + right;
-}
-else if (right <= 0) {
-    maxPathThruCurrent = node.val + left;
-    toReturn = node.val + left;
-}
-else {
-    maxPathThruCurrent = node.val + left + right;
-    toReturn = node.val + Math.max(left, right);
-}
-
-ans = Math.max(ans, maxPathThruCurrent);
-return toReturn;      
-}*/
