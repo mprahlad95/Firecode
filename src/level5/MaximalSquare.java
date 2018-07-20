14fdaf6195755519f2a595158fdcd89de0eef3ab
Prahlad Misra <mprahlad95@gmail.com>
2018-07-20 17:13:55 +0530

    added

---
+++ src/level5/MaximalSquare.java
@@ -0,0 +1,19 @@
+package level5;
+
+public class MaximalSquare {
+	public static int largestSquare(char[][] matrix) {
+		if (matrix == null || matrix.length == 0)
+			return 0;
+		int max = 0;
+		int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
+		for (int i = 1; i <= matrix.length; i++) {
+			for (int j = 1; j <= matrix[0].length; j++) {
+				if (matrix[i - 1][j - 1] == '1') {
+					dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i - 1][j - 1], dp[i - 1][j]));
+					max = Math.max(dp[i][j], max);
+				}
+			}
+		}
+		return max * max;
+	}
+}
