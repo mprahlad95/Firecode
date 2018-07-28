800d0192f3404188709a03dbc418007df72d5e9c
Prahlad Misra <mprahlad95@gmail.com>
2018-07-28 13:34:56 +0530

    edit

--- src/level2/Transpose2DMatrix.java
+++ src/level2/Transpose2DMatrix.java
@@ -4,7 +4,7 @@ public class Transpose2DMatrix {
 	public static void transposeMatrix(int[][] matrix) {
 		int temp;
 		for (int i = 0; i < (matrix.length / 2) + 1; i++) {
-			for (int j = i; j < matrix[0].length; j++) {
+			for (int j = i; j < matrix[i].length; j++) {
 				temp = matrix[i][j];
 				matrix[i][j] = matrix[j][i];
 				matrix[j][i] = temp;
